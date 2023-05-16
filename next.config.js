e28792c91b7f26c89ebda5daca12eba51f13ee80
Le Khoa <lekhoa011159@gmail.com>
2023-05-16 13:48:38 +0700

    fix deploy

--- next.config.js
+++ next.config.js
@@ -22,6 +22,21 @@ const nextConfig = {
       },
     ],
   },
+
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
